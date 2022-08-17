92b0e43492a1e940aead62c8e8a2639769aa0ea7
AustynYou <austyn.you@gmail.com>
2022-08-17 00:36:37 -0700

    🚧 Implement input date

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
+  console.log(getStringDate(new Date()));
+  const [date, setDate] = useState();
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>This is New Diary page. </p>
+      <MyHeader
+        headText={"Write a New Diary"}
+        leftChild={<MyButton text={"< Go Back"} onClick={() => navigate(-1)} />}
+      />
+      <div>
+        <section>
+          <h4>What day is it?</h4>
+          <div className="input-box">
+            <input
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
